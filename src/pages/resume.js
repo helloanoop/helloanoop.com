6cf9330f657ff3f68c779ed34c850460cb1785e1
Anoop M D <anoop.md1421@gmail.com>
2023-05-10 15:22:32 +0530

    chore: updated resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -14,67 +14,57 @@ export default function Index() {
       <Header />
       <main className="page page-work">
         <div className="page-content">
-          <h4 className="text-xl font-semibold">Career Highlights ✨</h4>
+          <h4 className="text-xl">Career Highlights ✨</h4>
           <ul>
             <li>A decade of experience in programming and building apps with Javascript and web technologies</li>
-            <li>Co founded 2 startups and took them from zero to production</li>
+            <li>Creator of Bruno (Opensource IDE for exploring and testing apis) & the Bru Markup language - www.usebruno.com</li>
             <li>Strong leadership skills. Several years experience leading teams of 5-15 people.</li>
-            <li>Currently working as Engineering Manager for People10, leading a team of 12 delivering experiences for Allegiant Airlines</li>
+            <li>Co founded 2 companies and led product development from ideation to production.</li>
           </ul>
 
-          <h4 className="text-xl font-semibold mt-6">About Me 😎</h4>
-          <p>I am a full stack software engineer based out of Bangalore.</p>
-          <p>My interest in <Link href="/blog/battery-and-a-bulb">programming</Link> started with my fascination towards electronics as a kid. Over time, I came to the realisation that software is just a beautiful abstraction over the underlying transistors.</p>
-          <p>Over the last decade, I have been building software applications. My expertise primarily lies in Javascript.I make it my goal to create software with the user in mind, thus delivering intuitive user interface experiences.</p>
-          <p>I am obsessed with programming and building things.</p>
+          <h4 className="text-xl mt-6">Professional Experience 👨‍💻</h4>
 
-          <h4 className="text-xl font-semibold mt-6">Education 🎓</h4>
+          <div className="flex flex-col mt-4">
+            <h6 className="text-xl font-semibold">People10 Technosoft Pvt Ltd</h6>
+            <div className='text-sm'>Technical Architect - Jan 2016 - Mar 2023 (6yrs)</div>
+          </div>
+          <div className="mt-2">
+            During my 6-year tenure, I progressed from a senior software engineer to an architect. I was responsible for building and managing multiple in-house applications, developing customer-facing ecommerce websites, and creating frameworks that were adopted as standards across the organization for an American airlines company. I also led and managed several projects and mentored fellow developers, resulting in the growth and success of multiple teams within the organization.
+          </div>
+
+          <div className="flex flex-col mt-6">
+            <h6 className="text-xl font-semibold">Notebase Technologies LLP</h6>
+            <div className='text-sm'>Founder - Nov 2020 - Feb 2022 (1yr)</div>
+          </div>
+          <div className="mt-2">
+            My most spectacular failure, Me and my co-founder built a social graph based note taking app for a year, only to later realize that it did not solve a real problem.
+          </div>
+
+          <div className="flex flex-col mt-6">
+            <h6 className="text-xl font-semibold">SINI Labs Technologies LLP</h6>
+            <div className='text-sm'>Co-Founder - Dec 2014 - Mar 2020 (6yrs)</div>
+          </div>
+          <div className="mt-2">
+            Co-founded and bootstrapped the company to build a CRM software for salons and spas, which continues to be used by over 150 outlets in India. Led product development from ideation to production, gaining expertise in the entire tech stack including devops
+          </div>
+
+          <div className="flex flex-col mt-6">
+            <h6 className="text-xl font-semibold">Tata Elxsi</h6>
+            <div className='text-sm'>a Elxsi Senior Software Engineer - Jul 2012 - Dec 2014 (2yrs)</div>
+          </div>
+          <div className="mt-2">
+            Initially worked on firmware code in C for implementing 4G tech in mobile chipsets. Gained expertise in full stack web development during the second year, building web apps using NodeJs and Backbone.js.
+          </div>
+
+          <h4 className="text-xl mt-6">Education 🎓</h4>
           <ul>
             <li>B.Tech (2012) - Electronics & Communication Engg</li>
           </ul>
 
-          <h4 className="text-xl font-semibold mt-6">Professional Experience 👨‍💻</h4>
-          <div className="flex mt-2 items-center">
-            <h6 className="font-semibold">People10 Technosoft Pvt Ltd</h6>
-            <small className="ml-1">(4.5yrs)</small>
-          </div>
-          <ul>
-            <li>Engineering Manager - <small className="ml-1">Apr 2022 - till date</small></li>
-            <li>Engineering Lead - <small className="ml-1">Apr 2021 - Mar 2022</small></li>
-            <li>Technical Consultant - <small className="ml-1">Sep 2018 - Mar 2021</small></li>
-            <li>Technical Lead - <small className="ml-1">May 2017 - Aug 2017</small></li>
-            <li>Development Lead - <small className="ml-1">May 2016 - April 2017</small></li>
-            <li>Senior Software Engg - <small className="ml-1">Jan 2016 - April 2016</small></li>
-          </ul>
-
-          <div className="flex mt-2 items-center">
-            <h6 className="font-semibold">Tata Elxsi</h6>
-            <small className="ml-1">(2.5yrs)</small>
-          </div>
-          <ul>
-            <li>Senior Software Engg - <small className="ml-1">May 2014 - Nov 2014</small></li>
-            <li>Software Engg - <small className="ml-1">July 2012 - April 2014</small></li>
-          </ul>
-
-          <h4 className="text-xl font-semibold mt-6">Startups 🚀</h4>
-          <div className="flex mt-2 items-center">
-            <h6 className="font-semibold">Notebase Technologies LLP</h6>
-            <small className="ml-1">(1.5yrs)</small>
-          </div>
-          <ul>
-            <li>Founder - <small className="ml-1">Nov 2020 - Feb 2022</small></li>
-          </ul>
-          <p className="mt-2">Built a blogging platform based on graph based note taking.</p>
-
-          <div className="flex mt-6 items-center">
-            <h6 className="font-semibold">SINI Labs Technologies LLP</h6>
-            <small className="ml-1">(7yrs)</small>
-          </div>
-          <ul>
-            <li>Co Founder - <small className="ml-1">Dec 2014 - Aug 2022</small></li>
-          </ul>
-          <p className="mt-2">Led a team of 3 developers and built  <a target="_blank" rel="noreferrer" href="www.strizly.com">strizly.com</a>, A CRM for Salon & Spas used over by 150+ brands in India</p>
-
+          <h4 className="text-xl mt-6">About Me 😎</h4>
+          <p>My interest in <Link href="/blog/battery-and-a-bulb">programming</Link> started with my fascination towards electronics as a kid. Over time, I came to the realisation that software is just a beautiful abstraction over the underlying transistors.</p>
+          <p>Over the last decade, I have been building software applications. My expertise primarily lies in Javascript.I make it my goal to create software with the user in mind, thus delivering intuitive user interface experiences.</p>
+          <p>I am obsessed with programming and building things.</p>
         </div>
       </main>
     </>
