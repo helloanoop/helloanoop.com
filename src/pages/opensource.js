1558ed37974d45cef7ad14a35ef5213e0336b834
Anoop M D <anoop.md1421@gmail.com>
2025-03-30 22:05:36 +0530

    chore: updated my oss projects

--- src/pages/opensource.js
+++ src/pages/opensource.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Header from 'components/Header';
-import Link from 'next/link';
 
 export default function Opensource() {
   return (
@@ -23,25 +22,24 @@ export default function Opensource() {
           </div>
           <div className="mt-2">
             <span className='text-blue-500'>
-              <a href="https://github.com/helloanoop/commit-count" rel="noreferrer" target="_blank">commit-count</a>
-            </span> - See any GitHub repo's userwise commit count for the past 6 months
+              <a href="https://github.com/helloanoop/maya" rel="noreferrer" target="_blank">maya</a>
+            </span> - A hobby operating system
           </div>
           <div className="mt-2">
             <span className='text-blue-500'>
-              <a href="https://github.com/helloanoop/kgraph" rel="noreferrer" target="_blank">kgraph</a>
-            </span> - yet another graph based note taking app
-          </div>
-          <div className="mt-2">
-            <span className='text-blue-500'>
-              <a href="https://github.com/byte-42/redis-explorer" rel="noreferrer" target="_blank">redis explorer</a>
-            </span> - a lightweight redis gui to quickly search through keys
+              <a href="https://github.com/usebruno/httpfaker" rel="noreferrer" target="_blank">httpfaker</a>
+            </span> - A simple HTTP request and response testing service
           </div>
           <div className="mt-2">
             <span className='text-blue-500'>
               <a href="https://github.com/helloanoop/lru-map-cache" rel="noreferrer" target="_blank">lru-map-cache</a>
             </span> - a simple and fast lru cache in javascript
           </div>
-          
+          <div className="mt-2">
+            <span className='text-blue-500'>
+              <a href="https://github.com/helloanoop/commit-count" rel="noreferrer" target="_blank">commit-count</a>
+            </span> - See any GitHub repo's userwise commit count for the past 6 months
+          </div>
         </div>
       </main>
     </>
